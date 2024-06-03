ed5c725e80bed5cdee89f4e75d65724f3a428468
Richard Nordström <git.nordstrom@gmail.com>
2024-06-03 20:10:11 +0200

    add alias lookup

--- main.go
+++ main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
 	"os"
+	"strings"
 )
 
-func main() {
+type IpAlias struct {
+	Alias string `json:"alias"`
+	Ip    string `json:"ip"`
+}
+
+type Output struct {
+	Ip string
+}
+
+func get_my_ip() (ip string, err error) {
 	res, err := http.Get("https://icanhazip.com")
 	if err != nil {
 		fmt.Printf("error: %v\n", err)
@@ -15,11 +26,73 @@ func main() {
 	}
 	defer res.Body.Close()
 
+	if res.StatusCode != http.StatusOK {
+		fmt.Printf("error, status code: %v\n", res.StatusCode)
+		os.Exit(1)
+	}
 	body, err := io.ReadAll(res.Body)
 	if err != nil {
 		fmt.Printf("error: %v\n", err)
 		os.Exit(1)
 	}
-	s := string(body)
-	fmt.Printf("%s", s)
+
+	ip = strings.TrimSuffix(string(body), "\n")
+	return ip, nil
+}
+
+func get_aliases() ([]IpAlias, bool) {
+	home_dir, ok := os.LookupEnv("HOME")
+
+	if ok == false {
+		return []IpAlias{}, ok
+	}
+	s := []string{home_dir, ".myip.json"}
+	config_file := strings.Join(s, "/")
+	jsonFile, err := os.Open(config_file)
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+		return []IpAlias{}, false
+	}
+	defer jsonFile.Close()
+
+	byteValue, err := io.ReadAll(jsonFile)
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+		return []IpAlias{}, false
+	}
+
+	var aliases []IpAlias
+	err = json.Unmarshal(byteValue, &aliases)
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+		return []IpAlias{}, false
+	}
+
+	return aliases, true
+
+}
+func main() {
+	aliases, ok := get_aliases()
+	var ip_output Output
+
+	ip, err := get_my_ip()
+	if err != nil {
+		fmt.Printf("error: %v\n", err)
+		os.Exit(1)
+	}
+
+	// Look for alias
+	if ok == true {
+		for _, alias := range aliases {
+			if alias.Ip == ip {
+				ip_output.Ip = alias.Alias
+				break
+			}
+		}
+	}
+	if ip_output.Ip == "" {
+		ip_output.Ip = ip
+	}
+
+	fmt.Println(ip_output.Ip)
 }
